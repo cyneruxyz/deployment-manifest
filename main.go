0fc865fc52793cc0c64eb56e107de5b2d30b4734
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-19 23:15:33 +0800

    udt

--- main.go
+++ main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/shopspring/decimal"
 )
 
 var db *gorm.DB
 var err error
 
+// Product is a representation of a product
+type Product struct {
+	ID    int             `json:"id"`
+	Code  string          `json:"code"`
+	Name  string          `json:"name"`
+	Price decimal.Decimal `json:"price" sql:"type:decimal(16,2)"`
+}
+
+// Result is an array
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"name"`
+	Message string      `json:"message"`
+}
+
 func main() {
 	db, err = gorm.Open("mysql", "root:@/restapicrud_go?charset=utf8&parseTime=True")
+	if err != nil {
+		log.Println("Connection failed", err)
+	} else {
+		log.Println("Connection established")
+	}
 
+	db.AutoMigrate(&Product{})
+
+	handleRequest()
 }
+
+func handleRequest() {
+	log.Println("Start the development server at http://127.0.0.1:9999")
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":9999", myRouter))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
+}
+
+// https://levelup.gitconnected.com/build-a-rest-api-using-go-mysql-gorm-and-mux-a02e9a2865ee
